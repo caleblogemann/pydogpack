7a866ccc3aed529e3cf44c05018894b1ca941950
Caleb Logemann <logemann@iastate.edu>
2019-07-22 21:06:53 -0600

    Added Implicit Runge Kutta Time Stepping

---
+++ pydogpack/tests/implicit_runge_kutta_test.py
@@ -0,0 +1,17 @@
+from pydogpack.timestepping import implicit_runge_kutta as irk
+from pydogpack.timestepping import time_stepping
+from pydogpack.tests.utils import odes
+import numpy as np
+
+def test_backward_euler():
+    backward_euler = irk.BackwardEuler()
+    odes.sample_odes_implicit(backward_euler, 1)
+
+
+def test_crank_nicolson():
+    crank_nicolson = irk.CrankNicolson()
+    odes.sample_odes_implicit(crank_nicolson, 2)
+
+def test_irk2():
+    irk2 = irk.IRK2()
+    odes.sample_odes_implicit(irk2, 2)
