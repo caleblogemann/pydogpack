45f489e3a151b57309d8dbe3c0612503a995a4db
Caleb Logemann <logemann@iastate.edu>
2019-08-19 13:23:15 -0500

    Added utils for dg and ldg

---
+++ pydogpack/dg_utils.py
@@ -0,0 +1,162 @@
+from pydogpack.mesh import mesh
+from pydogpack.mesh import boundary
+from pydogpack.basis import basis
+from pydogpack.riemannsolvers import riemann_solvers
+from pydogpack.solution import solution
+import pydogpack.math_utils as math_utils
+
+import numpy as np
+
+
+# q_t + f(q)_x = 0
+# TODO: Add Source term
+# (Q_i)_t = (1/m_i) M^{-1} (dintt{D_i}{f(Q_i)\Phi_xi}{xi}
+# - (Fhat_{i+1/2} \Phi(1) - Fhat_{i-1/2}\Phi(-1)))
+def dg_weak_formulation(
+    q, flux_function, riemann_solver, boundary_condition=None, mesh_=None, basis_=None
+):
+    # q should be either a DGSolution object or array of coefficients
+    # if array of coefficients then mesh_ and basis_ are needed
+    if isinstance(q, solution.DGSolution):
+        dg_solution = q
+    else:
+        dg_solution = solution.DGSolution(q, basis_, mesh_)
+
+    if boundary_condition is None:
+        boundary_condition = boundary.Periodic()
+    if mesh_ is None:
+        mesh_ = dg_solution.mesh
+    if basis_ is None:
+        basis_ = dg_solution.basis
+
+    numerical_fluxes = np.zeros(mesh_.num_faces)
+    L = np.zeros((mesh_.num_elems, basis_.num_basis_cpts))
+
+    # M^{-1} \Phi(1.0)
+    m_inv_phi_1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(1.0))
+    # M^{-1} \Phi(-1.0)
+    m_inv_phi_m1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(-1.0))
+
+    for i in mesh_.boundary_faces:
+        numerical_fluxes[i] = boundary_condition.evaluate_boundary(
+            i, dg_solution, riemann_solver
+        )
+
+    for i in mesh_.interior_faces:
+        numerical_fluxes[i] = riemann_solver.solve()
+
+    for i in range(mesh_.num_elems):
+        left_face_index = mesh_.elems_to_faces[i, 0]
+        right_face_index = mesh_.elems_to_faces[i, 1]
+        quad_function = lambda xi: flux_function(
+            dg_solution.evaluate_canonical(xi, i)
+        ) * basis_.evaluate_gradient_canonical(xi)
+
+        # TODO: there may be better ways to compute this
+        # for example in the linear case this is already computed as stiffness matrix
+        int_f_q_phi_x = math_utils.quadrature(quad_function, -1.0, 1.0)
+        L[i, :] = (
+            1
+            / mesh_.elem_metrics[i]
+            * (
+                np.matmul(basis_.mass_matrix_inverse, int_f_q_phi_x)
+                - (
+                    numerical_fluxes[left_face_index] * m_inv_phi_1
+                    - numerical_fluxes[right_face_index] * m_inv_phi_m1
+                )
+            )
+        )
+    return L
+
+
+def dg_strong_formuation(
+    q, flux_function, riemann_solver, boundary_condition=None, mesh_=None, basis_=None
+):
+    return dg_formulation(
+        q,
+        flux_function,
+        riemann_solver,
+        boundary_condition,
+        mesh_,
+        basis_,
+        is_weak=False,
+    )
+
+
+# q_t + f(q)_x = 0
+# TODO: Add Source term
+# Weak Form
+# (Q_i)_t = (1/m_i) M^{-1} (dintt{D_i}{f(Q_i)\Phi_xi}{xi}
+# - (Fhat_{i+1/2} \Phi(1) - Fhat_{i-1/2}\Phi(-1)))
+# Strong Form
+# (Q_k)_t = (1/m_k) M^{-1} (-dintt{D_k}{f(Q_k)_xi\Phi}{xi}
+# + ((F(Q_k(1)) - Fhat_{k+1/2}) \Phi(1) - (F(Q_k(-1) - Fhat_{k-1/2})\Phi(-1)))
+def dg_formulation(
+    q,
+    flux_function,
+    riemann_solver,
+    boundary_condition=None,
+    mesh_=None,
+    basis_=None,
+    is_weak=True,
+):
+    # q should be either a DGSolution object or array of coefficients
+    # if array of coefficients then mesh_ and basis_ are needed
+    if isinstance(q, solution.DGSolution):
+        dg_solution = q
+    else:
+        dg_solution = solution.DGSolution(q, basis_, mesh_)
+
+    if boundary_condition is None:
+        boundary_condition = boundary.Periodic()
+    if mesh_ is None:
+        mesh_ = dg_solution.mesh
+    if basis_ is None:
+        basis_ = dg_solution.basis
+
+    numerical_fluxes = np.zeros(mesh_.num_faces)
+    L = np.zeros((mesh_.num_elems, basis_.num_basis_cpts))
+
+    # M^{-1} \Phi(1.0)
+    m_inv_phi_1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(1.0))
+    # M^{-1} \Phi(-1.0)
+    m_inv_phi_m1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(-1.0))
+
+    for i in mesh_.boundary_faces:
+        numerical_fluxes[i] = boundary_condition.evaluate_boundary(
+            i, dg_solution, riemann_solver
+        )
+
+    for i in mesh_.interior_faces:
+        numerical_fluxes[i] = riemann_solver.solve()
+
+    for i in range(mesh_.num_elems):
+        left_face_index = mesh_.elems_to_faces[i, 0]
+        right_face_index = mesh_.elems_to_faces[i, 1]
+        quad_function = lambda xi: flux_function(
+            dg_solution.evaluate_canonical(xi, i)
+        ) * basis_.evaluate_gradient_canonical(xi)
+
+        # TODO: there may be better ways to compute this
+        # for example in the linear case this is already computed as stiffness matrix
+        int_f_q_phi_x = math_utils.quadrature(quad_function, -1.0, 1.0)
+        L[i, :] = (
+            1
+            / mesh_.elem_metrics[i]
+            * (
+                np.matmul(basis_.mass_matrix_inverse, int_f_q_phi_x)
+                + (
+                    (
+                        flux_function(dg_solution.evaluate_canonical(1.0, i))
+                        - numerical_fluxes[left_face_index]
+                    )
+                    * m_inv_phi_1
+                    - (
+                        flux_function(dg_solution.evaluate_canonical(-1.0, i))
+                        - numerical_fluxes[right_face_index]
+                    )
+                    * m_inv_phi_m1
+                )
+            )
+        )
+    return L
---
+++ pydogpack/localdiscontinuousgalerkin/utils.py
@@ -0,0 +1,99 @@
+from pydogpack.riemannsolvers import riemann_solvers
+from pydogpack.mesh import boundary
+
+# Utility Functions/Classes for Local Discontinuous Galerkin Discretizations
+
+
+# represent numerical flux
+# Rhat = {R} + C_11 [Q] + C_12 [R]
+class DerivativeRiemannSolver(riemann_solvers.RiemannSolver):
+    def __init__(self, c11=0.0, c12=0.5):
+        self.c11 = c11
+        self.c12 = c12
+        riemann_solvers.RiemannSolver.__init__(self, lambda x: x)
+
+    def solve_states(self, left_state, right_state):
+        raise NotImplementedError(
+            "DerivativeRiemannSolver.solve_states is not implemented in favor of"
+            + " solve_dg_solution"
+        )
+
+    def solve_dg_solution(self, dg_solution, face_index):
+        basis_ = dg_solution.basis
+        left_elem = dg_solution.mesh.faces_to_elems[face_index, 0]
+        right_elem = dg_solution.mesh.faces_to_elems[face_index, 1]
+
+        left_state = dg_solution.evaluate_canonical(1.0, left_elem)
+        right_state = dg_solution.evaluate_canonical(-1.0, right_elem)
+        derivative_left_state = basis_.evaluate_canonical(
+            1.0, dg_solution.derivative, left_elem
+        )
+        derivative_right_state = basis_.evaluate_canonical(
+            -1.0, dg_solution.derivative, right_elem
+        )
+
+        derivative_average = self.interface_average(
+            derivative_left_state, derivative_right_state
+        )
+        derivative_jump = self.interface_jump(
+            derivative_left_state, derivative_right_state
+        )
+
+        jump = self.interface_jump(left_state, right_state)
+
+        return derivative_average + self.c11 * jump + self.c12 * derivative_jump
+
+
+# Qhat = {Q} - C_12 [Q]
+class RiemannSolver(riemann_solvers.RiemannSolver):
+    def __init__(self, c12=0.5):
+        self.c12 = c12
+        riemann_solvers.RiemannSolver.__init__(self, lambda x: x)
+
+    def solve_states(self, left_state, right_state):
+        average = self.interface_average(left_state, right_state)
+        jump = self.interface_jump(left_state, right_state)
+        return average - self.c12 * jump
+
+
+# boundary condition
+# Dirichlet - g_d(t) enforced at boundary for Q
+# Qhat = g_d - use standard dirichlet boundary object
+# Rhat = R^+ - C_11(Q^+ - g_d)n
+# Neumann - g_n(t) enforced at boundary for R
+# Qhat = Q^+ interior boundary
+# Rhat = g_n
+class DerivativeDirichlet(boundary.Dirichlet):
+    def __init__(self, boundary_function, c11=0.0):
+        self.c11 = c11
+
+        boundary.Dirichlet.__init__(self, boundary_function)
+
+    def evaluate_boundary(self, dg_solution, face_index, riemann_solver):
+        mesh_ = dg_solution.mesh
+        basis_ = dg_solution.basis
+
+        left_elem_index = mesh_.faces_to_elems[face_index, 0]
+        right_elem_index = mesh_.faces_to_elems[face_index, 1]
+
+        if left_elem_index != -1:
+            # right boundary
+            normal = 1.0
+            interior_state = dg_solution.evaluate_canonical(1.0, left_elem_index)
+            interior_derivative_state = basis_.evaluate_canonical(
+                1.0, dg_solution.derivative, left_elem_index
+            )
+        else:
+            # left boundary
+            normal = -1.0
+            interior_state = dg_solution.evaluate_canonical(-1.0, right_elem_index)
+            interior_derivative_state = basis_.evaluate_canonical(
+                -1.0, dg_solution.derivative, right_elem_index
+            )
+
+        vertex = dg_solution.mesh.vertices[face_index]
+        return (
+            interior_derivative_state
+            - self.c11 * (interior_state - self.boundary_function(vertex)) * normal
+        )
+
