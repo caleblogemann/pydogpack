5daffe27e510aca562326c4e7960ee0a59f7d8f1
Caleb Logemann <logemann@iastate.edu>
2019-08-19 13:17:54 -0500

    Added Euler app

---
+++ apps/euler/euler.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Euler:
+    def __init__(self, max_wavespeed):
+        self.max_wavespeed = max_wavespeed
+
+    def flux_function(self, u):
+        pass
+
+    def wavespeed_function(self, u):
+        pass
