45ddc8b02ff3fc6f0e3b741cd5234135749331cd
Caleb Logemann <logemann@iastate.edu>
2019-08-27 16:26:28 -0500

    finished writing dg formulations

--- apps/diffusion/ldg.py
+++ apps/diffusion/ldg.py
@@ -78,18 +78,21 @@ def ldg_operator(
     # Frequently used constants
     # M^{-1} S^T
     m_inv_s_t = basis_.mass_inverse_stiffness_transpose
+    quad_function = lambda i: dg_utils.matrix_quadrature_function(Q, m_inv_s_t, i)
     # M^{-1} \Phi(1.0)
     m_inv_phi_1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(1.0))
     # M^{-1} \Phi(-1.0)
     m_inv_phi_m1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(-1.0))
 
     FQ = dg_utils.evaluate_fluxes(Q, q_boundary_condition, q_numerical_flux)
-    R = dg_utils.evaluate_weak_form(Q, FQ, m_inv_s_t, m_inv_phi_m1, m_inv_phi_1)
+    R = dg_utils.evaluate_weak_form(Q, FQ, quad_function, m_inv_phi_m1, m_inv_phi_1)
+
     # store reference to Q in R
     # used in riemann_solvers/fluxes and boundary conditions for R sometimes depend on Q
     R.integral = Q.coeffs
+    quad_function = lambda i: dg_utils.matrix_quadrature_function(R, m_inv_s_t, i)
     FR = dg_utils.evaluate_fluxes(R, r_boundary_condition, r_numerical_flux)
-    L = dg_utils.evaluate_weak_form(R, FR, m_inv_s_t, m_inv_phi_m1, m_inv_phi_1)
+    L = dg_utils.evaluate_weak_form(R, FR, quad_function, m_inv_phi_m1, m_inv_phi_1)
 
     return L
 
--- pydogpack/dg_utils.py
+++ pydogpack/dg_utils.py
@@ -1,113 +1,50 @@
-from pydogpack.mesh import mesh
 from pydogpack.mesh import boundary
-from pydogpack.basis import basis
-from pydogpack.riemannsolvers import riemann_solvers
 from pydogpack.solution import solution
 import pydogpack.math_utils as math_utils
 
 import numpy as np
 
 
-# q_t + f(q)_x = 0
-# TODO: Add Source term
-# (Q_i)_t = (1/m_i) M^{-1} (dintt{D_i}{f(Q_i)\Phi_xi}{xi}
-# - (Fhat_{i+1/2} \Phi(1) - Fhat_{i-1/2}\Phi(-1)))
 def dg_weak_formulation(
-    q, flux_function, riemann_solver, boundary_condition=None, mesh_=None, basis_=None
-):
-    # q should be either a DGSolution object or array of coefficients
-    # if array of coefficients then mesh_ and basis_ are needed
-    if isinstance(q, solution.DGSolution):
-        dg_solution = q
-    else:
-        dg_solution = solution.DGSolution(q, basis_, mesh_)
-
-    if boundary_condition is None:
-        boundary_condition = boundary.Periodic()
-    if mesh_ is None:
-        mesh_ = dg_solution.mesh_
-    if basis_ is None:
-        basis_ = dg_solution.basis_
-
-    numerical_fluxes = np.zeros(mesh_.num_faces)
-    L = np.zeros((mesh_.num_elems, basis_.num_basis_cpts))
-
-    # M^{-1} \Phi(1.0)
-    m_inv_phi_1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(1.0))
-    # M^{-1} \Phi(-1.0)
-    m_inv_phi_m1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(-1.0))
-
-    for i in mesh_.boundary_faces:
-        numerical_fluxes[i] = boundary_condition.evaluate_boundary(
-            i, dg_solution, riemann_solver
-        )
-
-    for i in mesh_.interior_faces:
-        numerical_fluxes[i] = riemann_solver.solve()
-
-    for i in range(mesh_.num_elems):
-        left_face_index = mesh_.elems_to_faces[i, 0]
-        right_face_index = mesh_.elems_to_faces[i, 1]
-        quad_function = lambda xi: flux_function(
-            dg_solution.evaluate_canonical(xi, i)
-        ) * basis_.evaluate_gradient_canonical(xi)
-
-        # TODO: there may be better ways to compute this
-        # for example in the linear case this is already computed as stiffness matrix
-        int_f_q_phi_x = math_utils.quadrature(quad_function, -1.0, 1.0)
-        L[i, :] = (
-            1
-            / mesh_.elem_metrics[i]
-            * (
-                np.matmul(basis_.mass_matrix_inverse, int_f_q_phi_x)
-                - (
-                    numerical_fluxes[left_face_index] * m_inv_phi_1
-                    - numerical_fluxes[right_face_index] * m_inv_phi_m1
-                )
-            )
-        )
-    return L
-
-
-def dg_strong_formuation(
-    q, flux_function, riemann_solver, boundary_condition=None, mesh_=None, basis_=None
+    dg_solution, flux_function, riemann_solver, boundary_condition=None
 ):
     return dg_formulation(
-        q,
-        flux_function,
-        riemann_solver,
-        boundary_condition,
-        mesh_,
-        basis_,
-        is_weak=False,
+        dg_solution, flux_function, riemann_solver, boundary_condition
     )
 
 
-# q_t + f(q)_x = 0
-# TODO: Add Source term
-# Weak Form
-# (Q_i)_t = (1/m_i) M^{-1} (dintt{D_i}{f(Q_i)\Phi_xi}{xi}
-# - (Fhat_{i+1/2} \Phi(1) - Fhat_{i-1/2}\Phi(-1)))
-# Strong Form
-# (Q_k)_t = (1/m_k) M^{-1} (-dintt{D_k}{f(Q_k)_xi\Phi}{xi}
-# + ((F(Q_k(1)) - Fhat_{k+1/2}) \Phi(1) - (F(Q_k(-1) - Fhat_{k-1/2})\Phi(-1)))
+def dg_strong_formulation(
+    dg_solution,
+    flux_function,
+    flux_function_derivative,
+    riemann_solver,
+    boundary_condition=None,
+):
+    return dg_formulation(
+        dg_solution,
+        flux_function,
+        riemann_solver,
+        boundary_condition,
+        is_weak=False,
+        flux_function_derivative=flux_function_derivative,
+    )
+
+
 def dg_formulation(
     dg_solution,
     flux_function,
     riemann_solver,
     boundary_condition=None,
     is_weak=True,
+    flux_function_derivative=None,
 ):
     # Default to periodic boundary conditions
     if boundary_condition is None:
         boundary_condition = boundary.Periodic()
 
-    mesh_ = dg_solution.mesh
-    basis_ = dg_solution.basis_
+    basis_ = dg_solution.basis
 
-    numerical_fluxes = np.zeros(mesh_.num_faces)
-    L = np.zeros((mesh_.num_elems, basis_.num_basis_cpts))
-
+    # left and right vectors
     # M^{-1} \Phi(1.0)
     m_inv_phi_1 = np.matmul(basis_.mass_matrix_inverse, basis_.evaluate(1.0))
     # M^{-1} \Phi(-1.0)
@@ -115,37 +52,32 @@ def dg_formulation(
 
     numerical_fluxes = evaluate_fluxes(dg_solution, boundary_condition, riemann_solver)
 
+    # TODO: could add check for linear flux
+    # where quadrature is already computed as part of basis
+    if is_weak:
+        def quadrature_function(i):
+            return compute_quadrature_weak(dg_solution, flux_function, i)
 
-    for i in range(mesh_.num_elems):
-        left_face_index = mesh_.elems_to_faces[i, 0]
-        right_face_index = mesh_.elems_to_faces[i, 1]
-        quad_function = lambda xi: flux_function(
-            dg_solution.evaluate_canonical(xi, i)
-        ) * basis_.evaluate_gradient_canonical(xi)
-
-        # TODO: there may be better ways to compute this
-        # for example in the linear case this is already computed as stiffness matrix
-        int_f_q_phi_x = math_utils.quadrature(quad_function, -1.0, 1.0)
-        L[i, :] = (
-            1
-            / mesh_.elem_metrics[i]
-            * (
-                np.matmul(basis_.mass_matrix_inverse, int_f_q_phi_x)
-                + (
-                    (
-                        flux_function(dg_solution.evaluate_canonical(1.0, i))
-                        - numerical_fluxes[left_face_index]
-                    )
-                    * m_inv_phi_1
-                    - (
-                        flux_function(dg_solution.evaluate_canonical(-1.0, i))
-                        - numerical_fluxes[right_face_index]
-                    )
-                    * m_inv_phi_m1
-                )
-            )
+        transformed_solution = evaluate_weak_form(
+            dg_solution,
+            numerical_fluxes,
+            quadrature_function,
+            m_inv_phi_m1,
+            m_inv_phi_1,
         )
-    return L
+    else:
+        def quadrature_function(i):
+            return compute_quadrature_strong(dg_solution, flux_function_derivative, i)
+
+        transformed_solution = evaluate_strong_form(
+            dg_solution,
+            flux_function,
+            numerical_fluxes,
+            quadrature_function,
+            m_inv_phi_m1,
+            m_inv_phi_1,
+        )
+    return transformed_solution
 
 
 def evaluate_fluxes(dg_solution, boundary_condition, numerical_flux):
@@ -160,8 +92,20 @@ def evaluate_fluxes(dg_solution, boundary_condition, numerical_flux):
     return F
 
 
+# q_t + f(q)_x = 0
+# TODO: add source term
+# m_i M Q_t = \dintt{D_i}{f(Q) \phi_xi}{xi} - (\phi(1) F_{i+1/2} - \phi(-1) F_{i-1/2})
+# Q_t = 1/m_i M^{-1} \dintt{D_i}{f(Q) \phi_xi}{xi}
+#   - 1/m_i M^{-1} (\phi(1) F_{i+1/2} - \phi(-1) F_{i-1/2})
+# (Q_i)_t = 1/m_i * quadrature_matrix * Q_i
+# - 1/m_i(vector_right F_{i+1/2} - vector_left F_{i-1/2}))
+# dg_solution = Q, with mesh and basis
+# numerical_fluxes = F, F_{i+1/2}, F_{i-1/2}
+# quadrature_function = M^{-1} \dintt{D_i}{F(Q_i) \phi_xi}{xi}
+# vector_left = M^{-1} \phi(-1)
+# vector_right = M^{-1} \phi(1)
 def evaluate_weak_form(
-    dg_solution, numerical_fluxes, quadrature_matrix, vector_left, vector_right
+    dg_solution, numerical_fluxes, quadrature_function, vector_left, vector_right
 ):
     mesh_ = dg_solution.mesh
     basis_ = dg_solution.basis
@@ -179,17 +123,39 @@ def evaluate_weak_form(
             1.0
             / mesh_.elem_metrics[i]
             * (
-                -1.0 * np.matmul(quadrature_matrix, dg_solution[i])
-                + numerical_fluxes[right_face_index] * vector_right
-                - numerical_fluxes[left_face_index] * vector_left
+                quadrature_function(i)
+                - (
+                    numerical_fluxes[right_face_index] * vector_right
+                    - numerical_fluxes[left_face_index] * vector_left
+                )
             )
         )
 
     return transformed_solution
 
 
+# q_t + f(q)_x = 0
+# TODO: add source term
+# m_i M Q_t = \dintt{D_i}{f(Q) \phi_xi}{xi} - (\phi(1) F_{i+1/2} - \phi(-1) F_{i-1/2})
+# m_i M Q_t = -\dintt{D_i}{f(Q)_xi \phi}{xi}
+#   + (\phi(1)(f(Q_{i+1/2}) - F_{i+1/2}) - \phi(-1)(f(Q_{i-1/2} - F_{i-1/2}))
+# Q_t = -1/m_i M^{-1} \dintt{D_i}{f(Q)_xi \phi}{xi}
+# + 1/m_i M^{-1} (\phi(1)(f(Q_{i+1/2}) - F_{i+1/2}) - \phi(-1)(f(Q_{i-1/2} - F_{i-1/2}))
+# (Q_i)_t = 1/m_i * -1.0 * quadrature_matrix * Q_i
+# + 1/m_i(vector_right(f(Q_{i+1/2}) - F_{i+1/2}) - vector_left(f(Q_{i-1/2} - F_{i-1/2}))
+# dg_solution = Q, with mesh and basis
+# flux_function = f
+# numerical_fluxes = F, F_{i+1/2}, F_{i-1/2}
+# quadrature_function = M^{-1} \dintt{D_i}{f(Q_i)_xi \phi}{xi}
+# vector_left = M^{-1} \phi(-1)
+# vector_right = M^{-1} \phi(1)
 def evaluate_strong_form(
-    dg_solution, numerical_fluxes, quadrature_matrix, vector_left, vector_right
+    dg_solution,
+    flux_function,
+    numerical_fluxes,
+    quadrature_function,
+    vector_left,
+    vector_right,
 ):
     mesh_ = dg_solution.mesh
     basis_ = dg_solution.basis
@@ -207,10 +173,70 @@ def evaluate_strong_form(
             1.0
             / mesh_.elem_metrics[i]
             * (
-                -1.0 * np.matmul(quadrature_matrix, dg_solution[i])
-                + numerical_fluxes[right_face_index] * vector_right
-                - numerical_fluxes[left_face_index] * vector_left
+                -1.0 * quadrature_function(i)
+                + (
+                    (
+                        flux_function(dg_solution.evaluate_canonical(1.0, i))
+                        - numerical_fluxes[right_face_index]
+                    )
+                    * vector_right
+                    - (
+                        flux_function(dg_solution.evaluate_canonical(-1.0, i))
+                        - numerical_fluxes[left_face_index]
+                    )
+                    * vector_left
+                )
             )
         )
 
     return transformed_solution
+
+def compute_quadrature_weak(dg_solution, flux_function, i):
+    basis_ = dg_solution.basis
+    result = np.zeros(basis_.num_basis_cpts)
+
+    # if first order then will be zero
+    if (basis_.num_basis_cpts == 1):
+        return 0.0
+
+    for l in range(basis_.num_basis_cpts):
+
+        def quadrature_function(xi):
+            return flux_function(
+                dg_solution.evaluate_canonical(xi, i)
+            ) * basis_.evaluate_gradient_canonical(xi, l)
+
+        result[l] = math_utils.quadrature(quadrature_function, -1.0, 1.0)
+
+    result = np.matmul(basis_.mass_matrix_inverse, result)
+
+    return result
+
+
+def compute_quadrature_strong(dg_solution, flux_function_derivative, i):
+    basis_ = dg_solution.basis
+    result = np.zeros(basis_.num_basis_cpts)
+
+    # if first order then will be zero
+    if (basis_.num_basis_cpts == 1):
+        return 0.0
+
+    for l in range(basis_.num_basis_cpts):
+
+        def quadrature_function(xi):
+            return (
+                flux_function_derivative(dg_solution.evaluate_canonical(xi, i))
+                * dg_solution.evaluate_gradient_canonical(xi, i)
+                * basis_.evaluate_canonical(xi, l)
+            )
+
+        result[l] = math_utils.quadrature(quadrature_function, -1.0, 1.0)
+
+    result = np.matmul(basis_.mass_matrix_inverse, result)
+
+    return result
+
+
+# useful if quadrature function is just a matrix multiplication
+def matrix_quadrature_function(dg_solution, matrix, i):
+    return np.matmul(matrix, dg_solution[i])
