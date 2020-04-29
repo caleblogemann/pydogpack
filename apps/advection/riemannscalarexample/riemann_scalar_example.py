0d752b1b2d4374d39d592c6aeb925c9ecfe436f7
Caleb Logemann <logemann@iastate.edu>
2020-04-29 13:34:42 -0500

    Updated riemann scalar advection example

--- apps/advection/riemannscalarexample/riemann_scalar_example.py
+++ apps/advection/riemannscalarexample/riemann_scalar_example.py
@@ -1,24 +1,32 @@
 from apps import problem
 from apps.advection import advection
-from pydogpack.utils import x_functions
 from pydogpack import main
+from pydogpack.utils import x_functions
 
 
 class RiemannScalarExample(problem.Problem):
     def __init__(
-        self, wavespeed=1.0, left_state=1.0, right_state=-1.0, source_function=None
+        self,
+        wavespeed=1.0,
+        left_state=1.0,
+        right_state=-1.0,
+        discontinuity_location=0.0,
+        source_function=None,
     ):
         self.wavespeed = wavespeed
         self.left_state = left_state
         self.right_state = right_state
+        self.discontinuity_location = discontinuity_location
 
-        app = advection.Advection(wavespeed, source_function)
-        initial_condition = x_functions.RiemannProblem(left_state, right_state)
+        app_ = advection.Advection(wavespeed, source_function)
+        initial_condition = x_functions.RiemannProblem(
+            left_state, right_state, discontinuity_location
+        )
         max_wavespeed = wavespeed
         exact_solution = advection.ExactSolution(initial_condition, self.wavespeed)
 
-        problem.Problem.__init__(
-            app, initial_condition, source_function, max_wavespeed, exact_solution
+        super().__init__(
+            app_, initial_condition, source_function, max_wavespeed, exact_solution
         )
 
 
@@ -26,5 +34,8 @@ if __name__ == "__main__":
     wavespeed = 1.0
     left_state = 1.0
     right_state = -1.0
-    problem = RiemannScalarExample(wavespeed, left_state, right_state)
+    discontinuity_location = -0.5
+    problem = RiemannScalarExample(
+        wavespeed, left_state, right_state, discontinuity_location
+    )
     main.run(problem)
