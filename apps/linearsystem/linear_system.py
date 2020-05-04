7cff9f9a7d960fd25b7df6619fe38f258a737bd1
Caleb Logemann <logemann@iastate.edu>
2020-05-04 15:32:07 -0500

    added exact solution and operators to linearsystem

--- apps/linearsystem/linear_system.py
+++ apps/linearsystem/linear_system.py
@@ -2,6 +2,8 @@ from apps import app
 from pydogpack.utils import flux_functions
 from pydogpack.utils import xt_functions
 
+import numpy as np
+
 
 class LinearSystem(app.App):
     # \v{q}_t + (A \v{q})_x = s(q, x, t)
@@ -28,27 +30,84 @@ class LinearSystem(app.App):
 
 class ExactSolution(xt_functions.XTFunction):
     # Exact Solution of Linear System \v{q}_t + (A \v{q})_x = \v{s}(\v{q}, x, t)
-    def __init__(self, q, matrix, source_function=None):
-        pass
+    # initial_condition should be x_function
+    # matrix - A
+    # source_function - \v{s}(\v{q}, x, t) or None if zero
+    # TODO: add effects of source_function
+    # Use characteristic variables to find exact solution
+    # A should be diagonalizable with real eigenvalues
+    # A = R Lambda R^{-1} = R Lambda L
+    # characteristic variables - w = R^{-1} q = L q
+    # L \v{q}_t + L R Lambda L q_x = 0
+    # \v{w}_t + Lambda \v{w}_x = 0
+    # characteristc variables are decoupled and advect
+    # with wavespeed determined by eigenvalues
+    def __init__(self, initial_condition, matrix, source_function=None):
+        self.initial_condition = initial_condition
+        self.matrix = matrix
+        self.source_function = source_function
+
+        self.num_eqns = self.matrix.shape[0]
+        # error checking
+        assert self.matrix.ndim == 2
+        assert self.num_eqns == self.matrix.shape[1]
+
+        self.eigenspace = np.linalg.eig(self.matrix)
+        self.eigenvalues = self.eigenspace[0]
+        self.eigenvectors_right = self.eigenspace[1]
+        self.eigenvectors_left = np.linalg.inv(self.eigenvectors_right)
+
+        self.R = self.eigenvectors_right
+        self.L = self.eigenvectors_left
 
     def function(self, x, t):
-        pass
+        # x could be list
+        # t should be scalar
+        # return shape should be (num_eqns, len(x))
+        if hasattr(x, "__len__"):
+            w = np.zeros((self.num_eqns, len(x)))
+        else:
+            w = np.zeros((self.num_eqns))
+        for i in range(self.num_eqns):
+            w[i] = self.L[i] @ self.initial_condition(x - self.eigenvalues[i] * t)
+
+        return self.R @ w
 
 
 class ExactOperator(xt_functions.XTFunction):
     # \v{L}(\v{q}) = \v{q}_t + (A \v{q})_x - \v{s}(\v{q}, x, t)
     def __init__(self, q, matrix, source_function=None):
-        pass
+        # q - xt_function, generally exact solution or initial condition
+        # matrix - A
+        # source_function - flux_function, \v{q}(\v{q}, x, t) or None if zero
+        self.q = q
+        self.matrix = matrix
+        self.source_function = source_function
 
     def function(self, x, t):
-        pass
+        q_t = self.q.t_derivative(x, t)
+        Aq_x = self.matrix @ self.q.x_derivative(x, t)
+        if self.source_function is not None:
+            s = self.source_function(self.q(x, t), x, t)
+            return q_t + Aq_x - s
+        else:
+            return q_t + Aq_x
 
 
 class ExactTimeDerivative(xt_functions.XTFunction):
     # \v{q}_t = \v{L}(\v{q})
     # \v{L}(\v{q}) = - (A \v{q})_x + \v{s}(\v{q}, x, t)
-    def __init__(self, matrix, source_function=None):
-        pass
+    def __init__(self, q, matrix, source_function=None):
+        # q - xt_function, generally exact solution or initial condition
+        # matrix - A
+        # source_function - flux_function, \v{q}(\v{q}, x, t) or None if zero
+        self.q = q
+        self.matrix = matrix
+        self.source_function = source_function
 
     def function(self, x, t):
-        pass
+        Aq_x = self.matrix @ self.q.x_derivative(x, t)
+        if self.source_function is not None:
+            return self.source_function(self.q(x, t), x, t) - Aq_x
+        else:
+            return -1.0 * Aq_x
