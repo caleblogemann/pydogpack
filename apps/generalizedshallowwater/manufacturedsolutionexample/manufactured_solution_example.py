0a62defc3b007076c8daf8cc5557ab0752d37773
Caleb Logemann <logemann@iastate.edu>
2021-01-19 11:14:03 -0600

    Updated generalized shallow water manufactured solution

--- apps/generalizedshallowwater/manufacturedsolutionexample/manufactured_solution_example.py
+++ apps/generalizedshallowwater/manufacturedsolutionexample/manufactured_solution_example.py
@@ -1,6 +1,10 @@
 from apps import problem
 from apps.generalizedshallowwater import generalized_shallow_water
 
+from pydogpack import main
+from pydogpack.utils import x_functions
+from pydogpack.utils import xt_functions
+
 
 class ManufacturedSolutionExample(problem.Problem):
     def __init__(
@@ -12,18 +16,70 @@ class ManufacturedSolutionExample(problem.Problem):
         kinematic_viscosity=generalized_shallow_water.DEFAULT_KINEMATIC_VISCOSITY,
         slip_length=generalized_shallow_water.DEFAULT_SLIP_LENGTH,
     ):
+        additional_source = generalized_shallow_water.ExactOperator(
+            exact_solution,
+            num_moments,
+            gravity_constant,
+            kinematic_viscosity,
+            slip_length,
+        )
         app_ = generalized_shallow_water.GeneralizedShallowWater(
-            num_moments, gravity_constant, kinematic_viscosity, slip_length
+            num_moments,
+            gravity_constant,
+            kinematic_viscosity,
+            slip_length,
+            additional_source,
         )
 
         initial_condition = x_functions.FrozenT(exact_solution, 0)
-        source_function = generalized_shallow_water.ExactOperator(exact_solution)
-        exact_operator = generalized_shallow_water.ExactOperator(exact_solution, source_function)
-        exact_time_derivative = generalized_shallow_water.ExactTimeDerivative(exact_solution, source_function)
 
-        super().__init__(app_, initial_condition, max_wavespeed)
+        exact_operator = generalized_shallow_water.ExactOperator(
+            exact_solution,
+            num_moments,
+            gravity_constant,
+            kinematic_viscosity,
+            slip_length,
+            additional_source,
+        )
+        exact_time_derivative = generalized_shallow_water.ExactTimeDerivative(
+            exact_solution,
+            num_moments,
+            gravity_constant,
+            kinematic_viscosity,
+            slip_length,
+            additional_source,
+        )
+
+        super().__init__(
+            app_,
+            initial_condition,
+            max_wavespeed,
+            exact_solution,
+            exact_operator,
+            exact_time_derivative,
+        )
 
 
 if __name__ == "__main__":
-    exact_solution = xt_functions
-    problem = ManufacturedSolutionExample()
+    num_moments = 1
+
+    gravity_constant = 1.0
+    kinematic_viscosity = 0.0
+    slip_length = 0.0
+
+    q1 = xt_functions.AdvectingSine(0.1, 1.0, 1.0, 0.0, 1.0)
+    q2 = xt_functions.AdvectingCosine(0.1, 1.0, 0.0, 0.0, 1.0)
+    q3 = xt_functions.AdvectingSine(0.1, 1.0, 0.0, 0.25, 1.0)
+    exact_solution = xt_functions.ComposedVector([q1, q2, q3])
+    max_wavespeed = 1.0
+
+    problem = ManufacturedSolutionExample(
+        exact_solution,
+        max_wavespeed,
+        num_moments,
+        gravity_constant,
+        kinematic_viscosity,
+        slip_length,
+    )
+
+    final_solution = main.run(problem)
